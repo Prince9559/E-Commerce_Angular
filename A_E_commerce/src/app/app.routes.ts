59a2911a538588483d119337e1003a96194960db
Prince Software <kumarprince95596186@gmail.com>
2026-05-30 14:50:38 +0530

    project

--- A_E_commerce/src/app/app.routes.ts
+++ A_E_commerce/src/app/app.routes.ts
@@ -12,31 +12,22 @@ import { Sports } from './pages/sports/sports';
 import { Books } from './pages/books/books';
 import { Cart } from './pages/cart/cart';
 import { ForYou } from './pages/for-you/for-you';
+import { Profile } from './pages/profile/profile';
 
 export const routes: Routes = [
 
   {path:'',component:ForYou},
-
   { path: 'home', component: Home },
-
   { path: 'fashion', component: Fashion },
-
   { path: 'mobiles', component: Mobiles },
-
   { path: 'beauty', component: Beauty },
-
   { path: 'electronics', component: Electronics },
-
   { path: 'home-appliances', component: HomeAppliances },
-
   { path: 'toys', component: Toys },
-
+  { path: 'profile', component: Profile},
   { path: 'food', component: Food },
-
   { path: 'sports', component: Sports },
-
   { path: 'books', component: Books },
-
   { path: 'cart', component: Cart }
 
 ];
